1fd266a040febc938961ca8f997510a3a86e82d6
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-05-05 14:57:18 -0300

    :recycle: refactor: Página de erro retorna para home
    Refatoração para página de erro, que agora joga o usuário de volta a home em alguns segundos

--- src/pages/Error/error-page.tsx
+++ src/pages/Error/error-page.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-
-interface RouteError {
-  statusText?: string;
-  message?: string;
-}
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <div id="error-page">
@@ -15,6 +20,8 @@ export default function ErrorPage() {
       <p>Desculpe, um erro inesperado aconteceu. 🌧️</p>
       <br/>
       <p>As vezes é necessário um pouco de chuva para se ter um lindo arco-íris. 🌈</p>
+      <br/>
+      <p>Você será redirecionado para a página inicial em alguns segundos. 🌞</p>
       <br/>
     </div>
   );

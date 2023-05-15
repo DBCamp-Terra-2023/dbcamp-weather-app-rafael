f8527c8f2c8d3a76a75144203fd409573c7b7677
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-05-15 20:27:04 -0300

    :sparkles: Asterisco para label dos campos obrigatórios
    Adicionado asterisco para label do componente de input nos campos obrigatórios

--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, {FC, useRef} from 'react'
 import './input.module.css';
 
 interface NewInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -6,13 +6,25 @@ interface NewInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   label?: string;
   register: any;
+  required?: boolean;
 }
 
-const Input: FC<NewInputProps> = ({type, className, register, label, ...props}) => {
+const Input: FC<NewInputProps> = ({type, className, register, label, required, ...props}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const labelText = required ? `${label}*` : label;
+
   return (
     <div>
-      <label htmlFor={className}>{label}</label>
-      <input className={className} type={type} {...props} {...register}/>
+      <label htmlFor={className}>
+        {labelText}
+      </label>
+      <input
+        className={className}
+        ref={inputRef}
+        type={type}
+        {...props}
+        {...register}
+      />
     </div>
   )
 }

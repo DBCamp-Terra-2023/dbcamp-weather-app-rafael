d7cdc9dd4b3a91dbd3cc3a4e46da33e89d62b903
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-05-18 16:16:32 -0300

    :recycle: Trocada descrição alt para melhor acessibilidade.

--- src/ui/components/Footer/index.tsx
+++ src/ui/components/Footer/index.tsx
@@ -5,7 +5,10 @@ import dblogo from '../../../assets/dblogo.svg';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>made with love </p><a href="https://db.tec.br"><img src={dblogo} alt="Logo da DB"/></a>
+      <p>made with love </p>
+      <a href="https://db.tec.br" target="_blank" rel="noopener noreferrer">
+        <img src={dblogo} alt="Logo da empresa DB Server"/>
+      </a>
     </footer>
   )
 }

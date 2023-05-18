9e0fd269039c4236419f4a809ff6520b4ed9e5c7
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-05-18 15:21:58 -0300

    :recycle: Adicionando acessibilidade e label com Span

--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -1,30 +1,50 @@
 import React, {FC, useRef} from 'react'
-import './input.module.css';
+import './input.css';
 
 interface NewInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: string;
   className?: string;
+  id?: string;
   label?: string;
-  register: any;
+  register?: any;
   required?: boolean;
+  spanValue?: string;
+  disableTab?: boolean;
 }
 
-const Input: FC<NewInputProps> = ({type, className, register, label, required, ...props}) => {
+const Input: FC<NewInputProps> = ({
+                                    type,
+                                    className,
+                                    register,
+                                    label,
+                                    required,
+                                    spanValue,
+                                    id,
+                                    disableTab = false,
+                                    ...props
+                                  }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const labelText = required ? `${label}*` : label;
 
   return (
-    <div>
-      <label htmlFor={className}>
-        {labelText}
-      </label>
-      <input
-        className={className}
-        ref={inputRef}
-        type={type}
-        {...props}
-        {...register}
-      />
+    <div className="inputContainer">
+      <div className="labelContainer">
+        <label htmlFor={id}>
+          {label}
+          {required && <span aria-hidden="true">*</span>}
+        </label>
+      </div>
+      <div className="inputAndSpan">
+        <input
+          id={id}
+          className={className}
+          ref={inputRef}
+          type={type}
+          {...props}
+          {...register}
+          tabIndex={disableTab ? -1 : undefined}
+        />
+        <span className='spanForData'>{spanValue}</span>
+      </div>
     </div>
   )
 }
